78606836605394036219a3032da2eb15e8937afb
ScurveyMonkey <104399992+ScurveyMonkey@users.noreply.github.com>
2022-06-09 23:54:53 -0400

    Version 1.01
    Added Rays for throwing scythe.

--- Reapers Creepers/Assets/Scripts/PlayerMovement.cs
+++ Reapers Creepers/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float _mSpeed = 5.0f;
-    [SerializeField] float jumpForce;
+    public GameObject projectilePrefab;
+    public GameObject reticle;
+    public Camera cam;
+    public float laserLength = 50.0f;
+
+    private float _mSpeed = 5.0f;
+    private float jumpForce = 5.5f;
     [SerializeField] bool _isGrounded;
 
     private Rigidbody2D rb;
@@ -17,16 +22,69 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Movement();
         Jumping();
 
-    }
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
+        Debug.DrawRay(ray.origin, ray.direction * laserLength, Color.red);
+
+        if (Physics.Raycast(ray, out hit, laserLength))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Instantiate(projectilePrefab, transform.position, Quaternion.LookRotation(ray.direction));
+            if (hit.collider != null)
+            {
+                hit.rigidbody.AddForceAtPosition(ray.direction * _mSpeed, hit.point);
+                Debug.Log("Hitting: " + hit.collider.name);
+            }
+
+        }
+
+       /* Movement();
+        Jumping();
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            shootBullet();
+        }
+        Vector3 reticlePosition = reticle.transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(reticlePosition, Vector2.right, laserLength);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Hitting: " + hit.collider.name);
+        }
+       Debug.DrawRay(new Vector3(transform.position.x, transform.position.y), new Vector3(Input.mousePosition.x, Input.mousePosition.y), Color.red);
+       */
+
+    }
+    /*private void FixedUpdate()
+    {
+        Vector3 reticlePosition = reticle.transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(reticlePosition, Vector2.right, laserLength);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Hitting: " + hit.collider.name);
+        }
+        Debug.DrawRay(new Vector3(transform.position.x,transform.position.y), new Vector3(Input.mousePosition.x, Input.mousePosition.y), Color.red);
+    }
+    */
     void OnCollisionEnter2D(Collision2D collision)
     {
         _isGrounded = true;
@@ -74,4 +132,11 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    /*public void shootBullet()
+    {
+
+        GameObject b = Instantiate(projectilePrefab) as GameObject;
+        b.transform.position = new Vector2(transform.position.x, Quaternion.LookRotation();
+    }
+    */
 }
---
+++ Reapers Creepers/Assets/Scripts/PointandShoot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointandShoot : MonoBehaviour
+{
+    private Vector3 target;
+    public GameObject reticle;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+
+
+        reticle.transform.position = new Vector2(target.x, target.y + 1.15f); 
+    }
+
+}
