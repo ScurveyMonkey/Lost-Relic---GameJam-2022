fc9e02b7ca3328421ee51d29f18a333435b54c7a
ScurveyMonkey <104399992+ScurveyMonkey@users.noreply.github.com>
2022-06-14 21:16:27 -0400

    --Version 1.11
    Began adding UI

--- Reapers Creepers/Assets/Scripts/GameManager.cs
+++ Reapers Creepers/Assets/Scripts/GameManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public bool _gameOver;
+    public Button restartButton;
+    public TextMeshProUGUI gameOverText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,19 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        _gameOver = true;
+
+    }
+
+    void StartGame()
+    {
+        _gameOver = false;
+        SceneManager.LoadScene(1);
     }
 }
